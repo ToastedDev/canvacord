e7b893e7d13fbea1369009e28eb1ca375b5c88ef
Not TCA <realtcatech@gmail.com>
2023-11-29 17:49:39 +0800

    fix postinstall script (again) and esm

--- index.js
+++ index.js
@@ -2,49 +2,51 @@ const Canvacord = require("./src/Canvacord");
 
 // load default fonts
 try {
-    Canvacord.registerFonts();
-} catch(e) {}
+  Canvacord.registerFonts();
+} catch (e) {}
 
 /**
  * @param {stirng} moduleName module name
  */
 function load(moduleName) {
-    try {
-        return require(moduleName);
-    } catch(e) {
-        return null;
-    }
+  try {
+    return require(moduleName);
+  } catch (e) {
+    return null;
+  }
 }
 
 const { author, version } = require("./package.json");
 
-module.exports = {
-    Canvas: Canvacord,
-    Canvacord,
-    ConvolutionMatrix: Canvacord.CONVOLUTION_MATRIX,
-    Rank: require("./src/Rank"),
-    Spotify: require("./src/Spotify"),
-    Welcomer: require("./src/Welcomer"),
-    Leaver: require("./src/Leaver"),
-    Greeting: require("./src/base/GreetingsCard"),
-    CaptchaGen: load("captcha-canvas") ? load("captcha-canvas").CaptchaGenerator : null,
-    FortniteShop: load("discord-canvas") ? load("discord-canvas").FortniteShop : null,
-    FortniteStats: load("discord-canvas") ? load("discord-canvas").FortniteStats : null,
-    Plugins: require("./src/Plugins"),
-    Util: require("./src/Util"),
-    Assets: require("./src/Assets"),
-    // MiScellaneous eXtensions
-    MSX: {
-        Brightness: require("./libs/Brightness"),
-        Convolute: require("./libs/Convolute"),
-        Darkness: require("./libs/Darkness"),
-        Greyscale: require("./libs/Greyscale"),
-        Invert: require("./libs/Invert"),
-        Sepia: require("./libs/Sepia"),
-        Threshold: require("./libs/Threshold"),
-        Trigger: require("./libs/Trigger")
-    },
-    write: Canvacord.write,
-    author,
-    version
-};
+module.exports.Canvas = Canvacord;
+module.exports.Canvacord = Canvacord;
+module.exports.ConvolutionMatrix = Canvacord.CONVOLUTION_MATRIX;
+module.exports.Rank = require("./src/Rank");
+module.exports.Spotify = require("./src/Spotify");
+module.exports.Welcomer = require("./src/Welcomer");
+module.exports.Leaver = require("./src/Leaver");
+module.exports.Greeting = require("./src/base/GreetingsCard");
+module.exports.CaptchaGen = load("captcha-canvas")
+  ? load("captcha-canvas").CaptchaGenerator
+  : null;
+module.exports.ForniteShop = load("discord-canvas")
+  ? load("discord-canvas").FortniteShop
+  : null;
+module.exports.FortniteStats = load("discord-canvas")
+  ? load("discord-canvas").FortniteStats
+  : null;
+module.exports.Plugins = require("./src/Plugins");
+module.exports.util = require("./src/Util");
+module.exports.Assets = require("./src/Assets");
+module.exports.MSX = {};
+module.exports.MSX.Brightness = require("./libs/Brightness");
+module.exports.MSX.Convolute = require("./libs/Convolute");
+module.exports.MSX.Darkness = require("./libs/Darkness");
+module.exports.MSX.Greyscale = require("./libs/Greyscale.js");
+module.exports.MSX.Invert = require("./libs/Invert");
+module.exports.MSX.Sepia = require("./libs/Sepia");
+module.exports.MSX.Threshold = require("./libs/Threshold");
+module.exports.MSX.Trigger = require("./libs/Trigger");
+module.exports.write = Canvacord.write;
+module.exports.author = author;
+module.exports.version = version;
